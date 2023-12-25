18de011336f3663d15a77f03edb83aee9c5c960c
macerie <sviluppo+git@sanguineti.net>
2023-12-25 02:03:13 +0100

    sc-date

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-const nativedate = {
-  name: "Native date",
-  sql_name: "date",
-  fieldviews: {
-    show: { isEdit: false, run: (s) => `${s}` },
-    edit: {
-      isEdit: true,
-      run: (nm, v, attrs, cls) =>
-        `<input type="date" class="form-control ${cls}" name="${nm}" id="input${nm}" ${
-          v || v === 0 ? `value="${v}"` : ""
-        }>`,
-    },
-  },
-  read: (v) => {
-    return v; 
+const {
+  input,
+  div,
+  text,
+  script,
+  domReady,
+  text_attr,
+} = require("@saltcorn/markup/tags");
+
+const nativeDate = {
+  type: "Date",
+  isEdit: true,
+  run: (nm, v, attrs, cls) => {
+    return (
+      input({
+        type: "date",
+        class: ["form-control", cls],
+        name: text_attr(nm),
+        placeholder: attrs.placeholder,
+        disabled: attrs.disabled,
+        id: `input${text_attr(nm)}${rndid}`,
+        ...(typeof v !== "undefined" &&
+          v !== null && {
+            value: text_attr(
+              typeof v === "string"
+                ? v
+                : v
+                ? (typeof v === "number" ? new Date(v) : v).toISOString()
+                : undefined
+            ),
+          }),
+      }) 
+    );
   },
 };
 
-module.exports = { sc_plugin_api_version: 1, types: [nativedate] };
+module.exports = {
+  sc_plugin_api_version: 1,
+  fieldviews: { nativeDate },
+  plugin_name: "sc-date"
+};
