f826db21b112002d2b6c0977549a36e58eba5c2a
macerie <sviluppo+git@sanguineti.net>
2023-12-25 08:52:15 +0100

    nativedate

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
 const {
   input,
   div,
+  a,
   text,
   script,
-  domReady,
   text_attr,
+  domReady,
+  style,
 } = require("@saltcorn/markup/tags");
 
-const nativeDate = {
+const nativeDate = (dt) => ({
   type: "Date",
   isEdit: true,
-  run: (nm, v, attrs, cls) => {
+  run: (nm, v, attrs, cls, required, field) => {
     return (
       input({
         type: "date",
-        class: ["form-control", cls],
         name: text_attr(nm),
-        placeholder: attrs.placeholder,
-        disabled: attrs.disabled,
         id: `input${text_attr(nm)}`,
-        ...(typeof v !== "undefined" &&
-          v !== null && {
-            value: text_attr(
-              typeof v === "string"
-                ? v
-                : v
-                ? (typeof v === "number" ? new Date(v) : v).toISOString()
-                : undefined
-            ),
-          }),
-      }) 
+        value: v,
+      })
     );
   },
-};
+});
 
 module.exports = {
   sc_plugin_api_version: 1,
   fieldviews: { nativeDate },
-  plugin_name: "sc-date",
-
+  plugin_name: "sc-date"
 };
